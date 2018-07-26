71f4e3f96ae08e7da69d19b69c35fd8fd55a4161
Curtis Hughes <James.Hughes@asurion.com>
2018-07-26 09:23:41 -0500

    working with webpack

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-// import { Button } from '../node_modules/storybook/dist/app.bundle.js';
 import { Button } from '../node_modules/dist/app.bundle.js';
-// import { Button } from '../dist/app.bundle';
 
 storiesOf('Button', module)
   .add('with text', () => (

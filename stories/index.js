5d113d1d59cb304f7c8cff38e1424291277c47aa
Curtis Hughes <James.Hughes@asurion.com>
2018-07-26 10:42:09 -0500

    updated webpack

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import { Button } from '../node_modules/dist/app.bundle.js';
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+    <Button onClick={() => console.log('clicked')}>Hello Button</Button>
   ))
   .add('with some emoji', () => (
-    <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+    <Button onClick={() => console.log('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
   ));

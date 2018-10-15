a1047998c473735b44ef1d6daa5db9e32feb8d07
Curtis Hughes <James.Hughes@asurion.com>
2018-10-15 12:09:53 -0500

    modified the export to use mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
 const path = require('path');
 
-const dist = path.resolve(__dirname, (process.env.NODE_ENV === 'production') ? 'dist': 'node_modules/dist');
-const src = path.resolve(__dirname, 'src');
-
 // Webpack configuration
-module.exports = {
-    entry: path.join(src, 'index.ts'),
-    output: {
-        path: dist,
-        filename: 'app.bundle.js',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/,
-                use: [{
-                    loader: 'url-loader',
-                }]
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.tsx?$/,
-                enforce: 'pre',
-                loader: 'tslint-loader',
-                options: {
-                    failOnHint: true,
-                    tsConfigFile: require('./tslint.json')
+module.exports = (env, argv) => {
+    const dist = path.resolve(__dirname, (argv.mode === 'production') ? 'dist' : 'node_modules/dist');
+    const src = path.resolve(__dirname, 'src');
+
+    return {
+        entry: path.join(src, 'index.ts'),
+        output: {
+            path: dist,
+            filename: 'app.bundle.js',
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
+        },
+        module: {
+            rules: [{
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
                 },
-                exclude: /node_modules/
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    },
+                {
+                    test: /\.(png|jp(e*)g|svg)$/,
+                    use: [{
+                        loader: 'url-loader',
+                    }]
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.tsx?$/,
+                    enforce: 'pre',
+                    loader: 'tslint-loader',
+                    options: {
+                        failOnHint: true,
+                        tsConfigFile: require('./tslint.json')
+                    },
+                    exclude: /node_modules/
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.jsx'],
+        },
+    }
 };
